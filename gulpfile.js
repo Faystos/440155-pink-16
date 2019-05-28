4b96cba3e16743cf8d46c668fdf3460bea86863c
Vladimir <fayst_@bk.ru>
2019-05-28 11:22:36 +0300

    закончил оптимизацию изображений

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,10 @@ var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 var csso = require("gulp-csso");
 
+
+
+var imagemin = require("gulp-imagemin");
+
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
     .pipe(plumber())
@@ -28,6 +32,18 @@ gulp.task("css", function () {
     .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
+
+
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3})
+    imagemin.jpegtran({progressive: true})
+    imagemin.svgo()
+ ]))
+   .pipe(gulp.dest("build /img"));
+});
+
 
 gulp.task("server", function () {
   server.init({
